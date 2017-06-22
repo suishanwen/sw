3befeffb8f914a712ccd5f3002b8f3090b48c0b8
suishanwen <suishanwen@icloud.com>
2017-06-22 15:11:06 +0800

    modify remote ip

--- src/main/java/com.sw/api/note/NoteResource.java
+++ src/main/java/com.sw/api/note/NoteResource.java
@@ -64,7 +64,7 @@ public class NoteResource {
         return userFacade.getAllPost();
     }
 
-    public static String getIpAddr(HttpServletRequest request) {
+    private static String getIpAddr(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
