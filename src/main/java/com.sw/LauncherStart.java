013b6fec0ba75698c15f72c3e7a6da7885ebbe55
suishanwen <suishanwen@icloud.com>
2017-07-08 00:25:51 +0800

    rebuild

--- src/main/java/com.sw/LauncherStart.java
+++ src/main/java/com.sw/LauncherStart.java
@@ -1,44 +1,44 @@
-package com.sw;
-
-import com.sw.base.servlet.InvalidRequestServlet;
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-import com.google.inject.servlet.GuiceFilter;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.DefaultHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-import javax.servlet.DispatcherType;
-import java.util.EnumSet;
-
-public class LauncherStart {
-    @Inject
-    public static void main(String[] args) throws Exception {
-        ResourceModule resourceModule = new ResourceModule();
-        Injector injector = Guice.createInjector(new AbstractModule() {
-            @Override
-            protected void configure() {
-                install(resourceModule);
-            }
-        });
-        Server server = new Server(8051);
-
-        ServletContextHandler servletHandler = new ServletContextHandler();
-        servletHandler.setContextPath("/");
-        servletHandler.addServlet(new ServletHolder(new InvalidRequestServlet()), "/*");
-        FilterHolder guiceFilter = new FilterHolder(injector.getInstance(GuiceFilter.class));
-        servletHandler.addFilter(guiceFilter, "/*", EnumSet.allOf(DispatcherType.class));
-        HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{servletHandler, new DefaultHandler()});
-
-        server.setHandler(handlers);
-        server.start();
-        server.join();
-    }
-}
+//package com.sw;
+//
+//import com.sw.base.servlet.InvalidRequestServlet;
+//import com.google.inject.AbstractModule;
+//import com.google.inject.Guice;
+//import com.google.inject.Inject;
+//import com.google.inject.Injector;
+//import com.google.inject.servlet.GuiceFilter;
+//import org.eclipse.jetty.server.Handler;
+//import org.eclipse.jetty.server.Server;
+//import org.eclipse.jetty.server.handler.DefaultHandler;
+//import org.eclipse.jetty.server.handler.HandlerList;
+//import org.eclipse.jetty.servlet.FilterHolder;
+//import org.eclipse.jetty.servlet.ServletContextHandler;
+//import org.eclipse.jetty.servlet.ServletHolder;
+//
+//import javax.servlet.DispatcherType;
+//import java.util.EnumSet;
+//
+//public class LauncherStart {
+//    @Inject
+//    public static void main(String[] args) throws Exception {
+//        ResourceModule resourceModule = new ResourceModule();
+//        Injector injector = Guice.createInjector(new AbstractModule() {
+//            @Override
+//            protected void configure() {
+//                install(resourceModule);
+//            }
+//        });
+//        Server server = new Server(8051);
+//
+//        ServletContextHandler servletHandler = new ServletContextHandler();
+//        servletHandler.setContextPath("/");
+//        servletHandler.addServlet(new ServletHolder(new InvalidRequestServlet()), "/*");
+//        FilterHolder guiceFilter = new FilterHolder(injector.getInstance(GuiceFilter.class));
+//        servletHandler.addFilter(guiceFilter, "/*", EnumSet.allOf(DispatcherType.class));
+//        HandlerList handlers = new HandlerList();
+//        handlers.setHandlers(new Handler[]{servletHandler, new DefaultHandler()});
+//
+//        server.setHandler(handlers);
+//        server.start();
+//        server.join();
+//    }
+//}
