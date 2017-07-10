4d414e8d91f62dc4fc03468a31ae350807f3de74
suishanwen <suishanwen@icloud.com>
2017-07-10 10:11:42 +0800

    re nav

--- src/main/webapp/note/javascript/nav-controller.js
+++ src/main/webapp/note/javascript/nav-controller.js
@@ -1,12 +1,10 @@
 var NavController = function ($scope, $location, noteService) {
-    setBgHeight();
-    $(window).resize(setBgHeight);
     $scope.toDoSomething = function (loc) {
-        $(window).unbind('resize',setBgHeight);
-        noteService.note=null;
+        noteService.note = null;
         $location.path('/' + loc)
     };
-    function setBgHeight(){
-        $(".nav-main")[0].style.height = window.innerHeight+'px';
-    }
+
+    (function () {
+        RENDERER.init();
+    })()
 };
