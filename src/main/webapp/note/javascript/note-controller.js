c8fe362638eb0517e61ed7746b32ffdf79303c1d
suishanwen <suishanwen@icloud.com>
2017-07-10 17:12:31 +0800

    add source

--- src/main/webapp/note/javascript/note-controller.js
+++ src/main/webapp/note/javascript/note-controller.js
@@ -1,33 +1,37 @@
-var NoteController = function ($scope, $http, $timeout, $location,noteService) {
-    var getNote=function(){
-        var id=getQueryString(window.location.hash,"thread");
-        if(!id){
-            id=parseInt(sessionStorage.getItem("id"));
-            if(isNaN(id)){
+var NoteController = function ($scope, $http, $timeout, $location, noteService) {
+    $scope.source = "";
+    var getNote = function () {
+        var source = getQueryString(window.location.hash, "source");
+        var id = getQueryString(window.location.hash, "thread");
+        if (!id) {
+            id = parseInt(sessionStorage.getItem("id"));
+            $scope.source = sessionStorage.getItem("source");
+            if (isNaN(id)) {
                 $location.path("/list");
                 return
             }
-        }else{
-            sessionStorage.setItem("id",id);
-            window.location.hash="#/note"
+        } else {
+            sessionStorage.setItem("id", id);
+            sessionStorage.setItem("source", source);
+            window.location.hash = "#/note"
         }
-        var url = noteService.server+"api/note/get?id="+id;
+        var url = noteService.server + "api/note/get?id=" + id;
         $http.get(url).success(function (data) {
-            $scope.note=data;
+            $scope.note = data;
             $scope.buttonShow = false;
-            $("#content")[0].innerHTML=$scope.note.content;
+            $("#content")[0].innerHTML = $scope.note.content;
         }).error(function (data) {
         });
     };
 
     getNote();
-    
-    $scope.editNote=function(note){
-        noteService.note=note;
+
+    $scope.editNote = function (note) {
+        noteService.note = note;
         $location.path("/note-edit");
     };
 
-    $scope.backList=function(){
+    $scope.backList = function () {
         $location.path("/list");
     };
 
