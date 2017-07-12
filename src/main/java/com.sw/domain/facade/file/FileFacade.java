4508b0249329f61b64439f18fe378b7495e5ffc6
suishanwen <suishanwen@icloud.com>
2017-07-12 11:31:03 +0800

    rebuild

--- src/main/java/com.sw/domain/facade/file/FileFacade.java
+++ src/main/java/com.sw/domain/facade/file/FileFacade.java
@@ -3,6 +3,8 @@ package com.sw.domain.facade.file;
 
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -10,12 +12,18 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class FileFacade {
+    private static Logger logger = LoggerFactory.getLogger(FileFacade.class);
     public static final String path = "/etc/nginx/html/file/";
+    public static final String home = "/home/";
 
     public void uploadFile(FormDataMultiPart multiPart) throws Exception {
         FormDataBodyPart file = multiPart.getField("file");
         InputStream ins = file.getValueAs(InputStream.class);
-        File f = new File(path + file.getContentDisposition().getFileName());
+        String fileName = file.getContentDisposition().getFileName();
+        fileName = new String(fileName.getBytes("ISO-8859-1"), "UTF-8");
+        String uploadPath = fileName.contains("sw-") ? home : path;
+        File f = new File(uploadPath + fileName);
+        logger.info("upload " + fileName + " to " + uploadPath);
         OutputStream os = new FileOutputStream(f);
         int bytesRead = 0;
         byte[] buffer = new byte[8192];
