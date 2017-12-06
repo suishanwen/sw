444f0c4820a2df00737ca9767e381823194582a6
suishanwen <suishanwen@icloud.com>
2017-12-06 11:29:32 +0800

    add react demo/note3 build by redux react ant.design

--- src/main/java/com.sw/ResourceModule.java
+++ src/main/java/com.sw/ResourceModule.java
@@ -27,7 +27,9 @@ import static com.sw.base.config.Configuration.config;
 @EmbeddedGrizzly(assets =
         {@EmbeddedGrizzly.Asset(uri = "/note", resource = "note"),
                 @EmbeddedGrizzly.Asset(uri = "/mine", resource = "mine"),
-                @EmbeddedGrizzly.Asset(uri = "/note2", resource = "note2")
+                @EmbeddedGrizzly.Asset(uri = "/note2", resource = "note2"),
+                @EmbeddedGrizzly.Asset(uri = "/note3", resource = "note3"),
+                @EmbeddedGrizzly.Asset(uri = "/demo", resource = "demo")
         }
 )
 public class ResourceModule extends ApplicationModule<JpaConfiguration> {
