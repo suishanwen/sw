e9458ad1809dd259f08e81192b3ea15c9e59499b
suishanwen <suishanwen@icloud.com>
2018-01-05 16:31:44 +0800

    reset ip

--- src/main/java/com.sw/api/note/NoteResource.java
+++ src/main/java/com.sw/api/note/NoteResource.java
@@ -1,6 +1,7 @@
 package com.sw.api.note;
 
 
+import com.sw.base.exception.BusinessException;
 import com.sw.domain.entity.note.Note;
 import com.sw.domain.facade.note.NoteFacade;
 import com.sw.domain.util.OnException;
@@ -19,89 +20,89 @@ import java.util.List;
 
 @Path("/note")
 public class NoteResource {
-    private static Logger logger = LoggerFactory.getLogger(NoteResource.class);
+	private static Logger logger = LoggerFactory.getLogger(NoteResource.class);
 
-    private NoteFacade noteFacade;
+	private NoteFacade noteFacade;
 
-    @Inject
-    public NoteResource(NoteFacade noteFacade) {
-        this.noteFacade = noteFacade;
-    }
+	@Inject
+	public NoteResource(NoteFacade noteFacade) {
+		this.noteFacade = noteFacade;
+	}
 
-    @GET
-    @OnException("getAllPostFailed")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<PostVo> get() {
-        return noteFacade.getAllPost();
-    }
+	@GET
+	@OnException("getAllPostFailed")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<PostVo> get() {
+		return noteFacade.getAllPost();
+	}
 
-    @POST
-    @Path("add")
-    @OnException("addNoteFailed")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Note add(Note note, @Context HttpServletRequest request) {
-        note.setIp(getIpAddr(request));
-        return noteFacade.addNote(note);
-    }
+	@POST
+	@Path("add")
+	@OnException("addNoteFailed")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Note add(Note note, @Context HttpServletRequest request) {
+		note.setIp(getIpAddr(request));
+		return noteFacade.addNote(note);
+	}
 
-    @POST
-    @Path("edit")
-    @OnException("editNoteFailed")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Note edit(Note note, @Context HttpServletRequest request) {
-        String ip = getIpAddr(request);
-        logger.info(String.join(" ", "ip:", ip, "to edit note:", note.getId().toString()));
-        if (ip != null && !ip.contains("36.110.123") && !ip.contains("121.42.239.141")) {
-            if (note.getIp() != null && !ip.equals(note.getIp())) {
-                return null;
-            }
-        }
-        note.setIp(ip);
-        return noteFacade.editNote(note);
-    }
+	@POST
+	@Path("edit")
+	@OnException("editNoteFailed")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Note edit(Note note, @Context HttpServletRequest request) {
+		String ip = getIpAddr(request);
+		logger.info(String.join(" ", "ip:", ip, "to edit note:", note.getId().toString()));
+		if (ip != null && !ip.contains("36.110.123") && !ip.contains("121.42.239.141") && !ip.contains("210.12.222")) {
+			if (note.getIp() != null && !ip.equals(note.getIp())) {
+				throw new BusinessException("当前IP没有编辑权限！");
+			}
+		}
+		note.setIp(ip);
+		return noteFacade.editNote(note);
+	}
 
-    @GET
-    @Path("get")
-    @OnException("getNoteFailed")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Note get(@QueryParam("id") Integer id) {
-        return noteFacade.getNote(id);
-    }
+	@GET
+	@Path("get")
+	@OnException("getNoteFailed")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Note get(@QueryParam("id") Integer id) {
+		return noteFacade.getNote(id);
+	}
 
-    @GET
-    @Path("recommend")
-    @OnException("getRecommendNoteFailed")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<PostVo> getRecommend() {
-        return noteFacade.getRecommend();
-    }
+	@GET
+	@Path("recommend")
+	@OnException("getRecommendNoteFailed")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<PostVo> getRecommend() {
+		return noteFacade.getRecommend();
+	}
 
-    @POST
-    @Path("enquiry")
-    @OnException("sendEnquiryFailed")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response sendEnquiry(EnquiryVO enquiryVO) {
-        noteFacade.sendEnquiry(enquiryVO);
-        return Response.ok().build();
-    }
+	@POST
+	@Path("enquiry")
+	@OnException("sendEnquiryFailed")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response sendEnquiry(EnquiryVO enquiryVO) {
+		noteFacade.sendEnquiry(enquiryVO);
+		return Response.ok().build();
+	}
 
-    private static String getIpAddr(HttpServletRequest request) {
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
-    }
+	private static String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("X-Forwarded-For");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
 }
