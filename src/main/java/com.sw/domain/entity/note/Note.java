21b63034b02047237388e43b5892a65746b9c88b
suishanwen <suishanwen@icloud.com>
2018-01-17 16:14:35 +0800

    modify database

--- src/main/java/com.sw/domain/entity/note/Note.java
+++ src/main/java/com.sw/domain/entity/note/Note.java
@@ -4,114 +4,115 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "note")
 public class Note {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String poster;
-    @Column(unique = true)
-    private String title;
-    @Column(length = 2000)
-    private String content;
-    @Column(name = "post_time")
-    private Date postTime;
-    @Column(name = "edit_time")
-    private Date editTime;
-    private String ip;
-    private String summary;
-    private String tag;
-    private Integer recommend;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String poster;
+	@Column(unique = true)
+	private String title;
+	@Column(length = 2000)
+	private String content;
+	@Column(name = "post_time")
+	private Date postTime;
+	@Column(name = "edit_time")
+	private Date editTime;
+	private String ip;
+	private String summary;
+	private String tag;
+	private Integer recommend;
 
 
-    public Note() {
-    }
+	public Note() {
+	}
 
-    public Note(String poster, String title, String content, Date postTime, Date editTime, String ip) {
-        this.poster = poster;
-        this.title = title;
-        this.content = content;
-        this.postTime = postTime;
-        this.editTime = editTime;
-        this.ip = ip;
-    }
+	public Note(String poster, String title, String content, Date postTime, Date editTime, String ip) {
+		this.poster = poster;
+		this.title = title;
+		this.content = content;
+		this.postTime = postTime;
+		this.editTime = editTime;
+		this.ip = ip;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getPoster() {
-        return poster;
-    }
+	public String getPoster() {
+		return poster;
+	}
 
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-    public Date getPostTime() {
-        return postTime;
-    }
+	public Date getPostTime() {
+		return postTime;
+	}
 
-    public void setPostTime(Date postTime) {
-        this.postTime = postTime;
-    }
+	public void setPostTime(Date postTime) {
+		this.postTime = postTime;
+	}
 
-    public Date getEditTime() {
-        return editTime;
-    }
+	public Date getEditTime() {
+		return editTime;
+	}
 
-    public void setEditTime(Date editTime) {
-        this.editTime = editTime;
-    }
+	public void setEditTime(Date editTime) {
+		this.editTime = editTime;
+	}
 
-    public String getIp() {
-        return ip;
-    }
+	public String getIp() {
+		return ip;
+	}
 
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
 
-    public String getSummary() {
-        return summary;
-    }
+	public String getSummary() {
+		return summary;
+	}
 
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
 
-    public String getTag() {
-        return tag;
-    }
+	public String getTag() {
+		return tag;
+	}
 
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
 
-    public Integer getRecommend() {
-        return recommend;
-    }
+	public Integer getRecommend() {
+		return recommend;
+	}
 
-    public void setRecommend(Integer recommend) {
-        this.recommend = recommend;
-    }
+	public void setRecommend(Integer recommend) {
+		this.recommend = recommend;
+	}
 }
