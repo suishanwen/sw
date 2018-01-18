0183da1533a4adb3a1c3ef2e2df3ab4a64dc923c
suishanwen <suishanwen@icloud.com>
2018-01-18 15:40:59 +0800

    bug fix

--- src/main/java/com.sw/domain/facade/note/NoteFacade.java
+++ src/main/java/com.sw/domain/facade/note/NoteFacade.java
@@ -14,69 +14,75 @@ import java.util.List;
 public class NoteFacade extends BaseFacade {
 
 
-    @Transactional
-    public Note addNote(Note note) {
-        note.setPostTime(new Date());
-        try {
-            entityManager.persist(note);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-        return note;
-    }
+	@Transactional
+	public Note addNote(Note note) {
+		note.setPostTime(new Date());
+		try {
+			entityManager.persist(note);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return note;
+	}
 
 
-    @Transactional
-    public Note editNote(Note note) {
-        note.setEditTime(new Date());
-        entityManager.merge(note);
-        return note;
-    }
+	@Transactional
+	public Note editNote(Note note) {
+		Note note0 = entityManager.find(Note.class, note.getId());
+		note0.setIp(note.getIp());
+		note0.setContent(note.getContent());
+		note0.setRecommend(note.getRecommend());
+		note0.setSummary(note.getSummary());
+		note0.setTag(note.getTag());
+		note0.setTitle(note.getTitle());
+		note0.setEditTime(new Date());
+		return entityManager.merge(note0);
+	}
 
 
-    public Note getNote(Integer id) {
-        return entityManager.find(Note.class, id);
-    }
+	public Note getNote(Integer id) {
+		return entityManager.find(Note.class, id);
+	}
 
-    public List<PostVo> getAllPost() {
-        String sql = "SELECT\n" +
-                "    id,\n" +
-                "    title,\n" +
-                "    post_time,\n" +
-                "    edit_time\n" +
-                "FROM\n" +
-                "    note\n" +
-                "ORDER BY\n" +
-                "    CASE\n" +
-                "        WHEN edit_time IS NULL THEN post_time\n" +
-                "        ELSE edit_time\n" +
-                "    END\n" +
-                "DESC";
-        return createNativeQuery(sql, Lists.newArrayList(), PostVo.class);
-    }
+	public List<PostVo> getAllPost() {
+		String sql = "SELECT\n" +
+				"    id,\n" +
+				"    title,\n" +
+				"    post_time,\n" +
+				"    edit_time\n" +
+				"FROM\n" +
+				"    note\n" +
+				"ORDER BY\n" +
+				"    CASE\n" +
+				"        WHEN edit_time IS NULL THEN post_time\n" +
+				"        ELSE edit_time\n" +
+				"    END\n" +
+				"DESC";
+		return createNativeQuery(sql, Lists.newArrayList(), PostVo.class);
+	}
 
-    public List<PostVo> getRecommend() {
-        String sql = "SELECT\n" +
-                "    id,\n" +
-                "    title,\n" +
-                "    poster,\n" +
-                "    summary,\n" +
-                "    tag,\n" +
-                "    post_time\n" +
-                "FROM\n" +
-                "    note\n" +
-                "WHERE\n" +
-                "    recommend = 1\n" +
-                "ORDER BY post_time DESC\n" +
-                "LIMIT 6";
-        return createNativeQuery(sql, Lists.newArrayList(), PostVo.class);
-    }
+	public List<PostVo> getRecommend() {
+		String sql = "SELECT\n" +
+				"    id,\n" +
+				"    title,\n" +
+				"    poster,\n" +
+				"    summary,\n" +
+				"    tag,\n" +
+				"    post_time\n" +
+				"FROM\n" +
+				"    note\n" +
+				"WHERE\n" +
+				"    recommend = 1\n" +
+				"ORDER BY post_time DESC\n" +
+				"LIMIT 6";
+		return createNativeQuery(sql, Lists.newArrayList(), PostVo.class);
+	}
 
-    public void sendEnquiry(EnquiryVO enquiryVO){
-        MailUtils cn = new MailUtils();
-        cn.setAddress("controlservice@sina.com", "suishanwen@icloud.com", enquiryVO.getName() + ":" + enquiryVO.getEmail() + ":" + enquiryVO.getSubject());
-        cn.send("smtp.sina.com", "controlservice@sina.com", "a123456", enquiryVO.getMessage());
+	public void sendEnquiry(EnquiryVO enquiryVO) {
+		MailUtils cn = new MailUtils();
+		cn.setAddress("controlservice@sina.com", "suishanwen@icloud.com", enquiryVO.getName() + ":" + enquiryVO.getEmail() + ":" + enquiryVO.getSubject());
+		cn.send("smtp.sina.com", "controlservice@sina.com", "a123456", enquiryVO.getMessage());
 
-    }
+	}
 }
